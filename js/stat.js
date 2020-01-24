4da9602a31b473f52ac5b72e6d7fb1add8d93869
Елья <piragof@gmail.com>
2020-01-24 18:06:25 +0300

    вносит правки по 1 заданию

--- js/stat.js
+++ js/stat.js
@@ -12,6 +12,7 @@ var BAR_WIDTH = 40;
 var BAR_SPACE = 50;
 var FONT_GAP = 15;
 var NUMBERS_GAP = 60;
+var BLACK_COLOR = '#000';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -32,8 +33,8 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-var getColor = function (i) {
-  if (i === 0) {
+var getColor = function (arr, i) {
+  if (arr[i] === 'Вы') {
     return 'rgba(255, 0, 0, 1)';
   } else {
     return 'hsla(240, 100%, 50%, ' + Math.random() + ')';
@@ -44,19 +45,18 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
 
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = BLACK_COLOR;
   ctx.fillText('Ура вы победили!', CLOUD_X + HORIZONTAL_GAP, CLOUD_Y + FONT_GAP);
   ctx.fillText('Список результатов:', CLOUD_X + HORIZONTAL_GAP, CLOUD_Y + FONT_GAP * 2);
 
-  var players = ['Вы', 'Кекс', 'Катя', 'Игорь'];
   var maxTime = getMaxElement(times);
 
-  for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + HORIZONTAL_GAP + (BAR_WIDTH + BAR_SPACE) * i, VERTICAL_GAP + FONT_GAP * 2 + BAR_HEIGHT);
-    ctx.fillStyle = getColor(i);
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillStyle = BLACK_COLOR;
+    ctx.fillText(names[i], CLOUD_X + HORIZONTAL_GAP + (BAR_WIDTH + BAR_SPACE) * i, VERTICAL_GAP + FONT_GAP * 2 + BAR_HEIGHT);
+    ctx.fillStyle = getColor(names, i);
     ctx.fillRect(CLOUD_X + HORIZONTAL_GAP + (BAR_WIDTH + BAR_SPACE) * i, BAR_HEIGHT - BAR_HEIGHT * times[i] / maxTime + VERTICAL_GAP + FONT_GAP, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = BLACK_COLOR;
     ctx.fillText(Math.round(times[i]), CLOUD_X + HORIZONTAL_GAP + (BAR_WIDTH + BAR_SPACE) * i, BAR_HEIGHT - BAR_HEIGHT * times[i] / maxTime + NUMBERS_GAP);
   }
 };
