404664b055d2b8eb4b1f10911f504a1eb259f10d
Елья <piragof@gmail.com>
2020-01-25 17:31:42 +0300

    меняет расчёт высоты колонок

--- js/stat.js
+++ js/stat.js
@@ -11,7 +11,7 @@ var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_SPACE = 50;
 var FONT_GAP = 15;
-var NUMBERS_GAP = 60;
+var RECT_Y = 230;
 var BLACK_COLOR = '#000';
 
 var renderCloud = function (ctx, x, y, color) {
@@ -42,21 +42,50 @@ var getColor = function (arr, i) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
+  renderCloud(
+      ctx,
+      CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP,
+      'rgba(0, 0, 0, 0.3)'
+  );
+  renderCloud(ctx,
+      CLOUD_X,
+      CLOUD_Y,
+      '#ffffff'
+  );
 
   ctx.fillStyle = BLACK_COLOR;
-  ctx.fillText('Ура вы победили!', CLOUD_X + HORIZONTAL_GAP, CLOUD_Y + FONT_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + HORIZONTAL_GAP, CLOUD_Y + FONT_GAP * 2);
+  ctx.fillText(
+      'Ура вы победили!',
+      CLOUD_X + HORIZONTAL_GAP,
+      CLOUD_Y + FONT_GAP
+  );
+  ctx.fillText(
+      'Список результатов:',
+      CLOUD_X + HORIZONTAL_GAP,
+      CLOUD_Y + FONT_GAP * 2
+  );
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = BLACK_COLOR;
-    ctx.fillText(names[i], CLOUD_X + HORIZONTAL_GAP + (BAR_WIDTH + BAR_SPACE) * i, VERTICAL_GAP + FONT_GAP * 2 + BAR_HEIGHT);
+    ctx.fillText(
+        names[i],
+        CLOUD_X + HORIZONTAL_GAP + (BAR_WIDTH + BAR_SPACE) * i,
+        VERTICAL_GAP + FONT_GAP * 2 + BAR_HEIGHT
+    );
     ctx.fillStyle = getColor(names, i);
-    ctx.fillRect(CLOUD_X + HORIZONTAL_GAP + (BAR_WIDTH + BAR_SPACE) * i, BAR_HEIGHT - BAR_HEIGHT * times[i] / maxTime + VERTICAL_GAP + FONT_GAP, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillRect(
+        CLOUD_X + HORIZONTAL_GAP + (BAR_WIDTH + BAR_SPACE) * i,
+        RECT_Y,
+        BAR_WIDTH,
+        -BAR_HEIGHT * times[i] / maxTime
+    );
     ctx.fillStyle = BLACK_COLOR;
-    ctx.fillText(Math.round(times[i]), CLOUD_X + HORIZONTAL_GAP + (BAR_WIDTH + BAR_SPACE) * i, BAR_HEIGHT - BAR_HEIGHT * times[i] / maxTime + NUMBERS_GAP);
+    ctx.fillText(
+        Math.round(times[i]),
+        CLOUD_X + HORIZONTAL_GAP + (BAR_WIDTH + BAR_SPACE) * i,
+        -BAR_HEIGHT * times[i] / maxTime + RECT_Y - FONT_GAP
+    );
   }
 };
